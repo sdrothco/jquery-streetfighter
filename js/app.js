fc73a44b70cd1e7a5f4a0d97c9d90cdb30a1d736
sdrothco <sdrothco@gmail.com>
2014-03-03 10:28:39 -0700

    Added splash screen, added error handling to events

--- js/app.js
+++ js/app.js
@@ -1,29 +1,70 @@
+var keyPressed = false;
+var mousePressed = false;
 $(document).ready(function() {
-	$('.ryu').on('mouseenter mouseleave', function() {
-		$('.ryu-still').toggle();
-		$('.ryu-ready').toggle();
+	var overRyu = false;
+	$('.title').delay(900).fadeOut();
+	$('.ryu').on('mouseenter', function() {
+		if(!keyPressed && !mousePressed) {
+			overRyu = true;
+			hideRyu();
+			$('.ryu-ready').show();
+		}
 	})
-	// .on('mouseleave', function() {
-	// 	$(this).find('.ryu-ready').hide();	
-	// 	$(this).find('.ryu-still').show();
-	// })
+	.on('mouseleave', function() {
+		if(!keyPressed) {
+			overRyu = false;
+			hideRyu();
+			$('.ryu-still').show();
+		}
+	})
 	.on('mousedown', function() {
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
-			.animate({'left': '300px'}, 400, function() {
-				$(this).hide().css({'left': '-209px'});
-			});
-
+		if(!keyPressed) {
+			playHadouken();
+			hideRyu();
+			mousePressed = true;
+			$('.ryu-throwing').show();
+			$('.hadouken').finish().show()
+				.animate({'left': '300px'}, 400, function() {
+					$(this).hide().css({'left': '-209px'});
+				});
+		}
 	})
 	.on('mouseup', function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-
+		if(!keyPressed) {
+			mousePressed = false;
+			hideRyu();
+			$('.ryu-ready').show();
+		}
 	});
-
+	$(document).on ('keypress keydown', function (e) {
+        if (e.which==88 && !keyPressed && !mousePressed) {
+			hideRyu();
+			keyPressed = true;
+			$('.ryu-cool').show();
+		}
+    })
+    .on('keyup', function (e) {
+		if (e.which==88 && !mousePressed) {
+			keyPressed = false;
+			hideRyu();
+			if (overRyu) { $('.ryu-ready').show(); }
+			else {  $('.ryu-still').show(); }
+		}
+    })
+    .on('mouseup', function() {
+		if(!keyPressed) {
+			mousePressed = false;
+		}
+	});
 });
+function hideRyu () {
+	if( !keyPressed ) {
+		$('.ryu-ready').hide();
+		$('.ryu-still').hide();
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
+	}
+}
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
